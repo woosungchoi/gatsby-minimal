d10c205395581098885a2d451b3db14f0cd1c77d
woosungchoi <solut2000@gmail.com>
2020-07-06 22:04:20 +0900

    change

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { Link } from "gatsby"
+import Layout from "./layout"
+import Title from "./title"
+import Listing from "./listing"
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
+import replaceSlashes from "../utils/replaceSlashes"
+// @ts-ignore
+import Hero from "../texts/hero"
+
+type PostsProps = {
+  posts: {
+    slug: string
+    title: string
+    date: string
+    excerpt: string
+    description: string
+    timeToRead?: number
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }[]
+}
+
+const Homepage = ({ posts }: PostsProps) => {
+  const { basePath, blogPath } = useMinimalBlogConfig()
+
+  return (
+    <Layout>
+      <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 } }}>
+        <Hero />
+      </section>
+      <Title text="Latest Posts">
+        <Link to={replaceSlashes(`/${basePath}/${blogPath}`)}>모든 글 보기</Link>
+      </Title>
+      <Listing posts={posts} />
+    </Layout>
+  )
+}
+
+export default Homepage
