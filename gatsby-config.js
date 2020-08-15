e9c9c133495c5e1e649a852c3b17f80ce1a3eead
woosungchoi <solut2000@gmail.com>
2020-08-15 14:52:45 +0900

    fix PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,6 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
-
         icons: [
           {
             src: `/android-chrome-192x192.png`,
